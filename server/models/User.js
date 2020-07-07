be1cfbcee21e341b482934a8f0764226110dcee8
keunseok seo <keunseokseo@keunseoknoMacBook-Pro.local>
2020-07-07 18:46:27 +0900

    backend접속오류

--- server/models/User.js
+++ server/models/User.js
@@ -37,49 +37,68 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function( next ) {
     let user = this;
 
-    if(user.isModified('password')) {
+    if (user.isModified('password')) {
 
         // 비밀번호를 암호화 시킨다.
 
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            if(err) return next(err);
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
 
             bcrypt.hash(user.password, salt, function(err, hash) {
-            if(err) return next(err);
-            user.password = hash;
-            next();
-            });
-        });
+            if(err) return next(err)
+            user.password = hash
+            next()
+            })
+        })
     } else {
-        next();
+        next()
     }
-});
+})
 
 
 userSchema.methods.comparePassword = function(plainPassword, cb) {
     bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-      if (err) return cb(err);
-      cb(null, isMatch);
-    });
-  };
+      if (err) return cb(err)
+      cb(null, isMatch)
+    })
+  }
 
 userSchema.methods.generateToken = function(cb) {
 
-    var user = this;
+    let user = this;
 
     // jsonwebtoken을 이용해서 token을 생성하기
-    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    let token = jwt.sign(user._id.toHexString(), 'secretToken');
     // user._id + 'secretToken' = token
     // ->
     // 'secretToken' -> user._id
 
-    user.token = token;
+    user.token = token
     user.save(function(err, user){
-        if(err) return cb(err);
-        cb(null, user);
-    });
-};
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
-const User = mongoose.model('User',userSchema);
+userSchema.statics.findByToken = function(token, cb) {
+    let user = this;
 
-module.exports = { User };
+    // 토큰을 decode 한다.
+
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인 
+
+
+        user.findOne({"_id": decoded, "token": token}, function (err, user){
+            if (err) return cb(err);
+            cb(null, user)
+        })
+    })
+
+
+}
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = { User }
