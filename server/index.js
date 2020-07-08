5bca44e117629dfdd311ac5efa7f1fb2761668a3
keunseok seo <keunseokseo@keunseoknoMacBook-Pro.local>
2020-07-08 17:29:17 +0900

    리엑트 로그인페이지

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -26,10 +25,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요 ~~~ 테스트'))
 
 
-app.get('/api/hello', (req,res) => {
-  res.send("안녕하세요!!")
-})
-
+app.get('/api/hello', (req,res) => res.send("h2!!"))
 
 
 app.post('/api/users/register', (req, res) => {
@@ -107,6 +103,8 @@ app.get('/api/users/logout', auth, (req, res) => {
       })
     })
 })
+
+const port = 5000
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
